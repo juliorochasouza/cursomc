29e1d74e33b8b06b00a43cc0bfac0a86a9f23270
juliocesarrocha <juliorochaml@gmail.com>
2018-11-06 22:48:32 -0200

    Produto e associação muitos para muitos

--- src/main/java/com/juliorocha/cursomc/CursomcApplication.java
+++ src/main/java/com/juliorocha/cursomc/CursomcApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.juliorocha.cursomc.domain.Categoria;
+import com.juliorocha.cursomc.domain.Produto;
 import com.juliorocha.cursomc.repositories.CategoriaRepository;
+import com.juliorocha.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
@@ -16,6 +18,8 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRespository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -23,12 +27,36 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		/*
+		 * INCLUI CATEGORIAS*/
+		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		/*
+		 * INCLUI PRODUTOS*/
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
+		/*
+		 * INCLUI RELACIONAMENTO DE CATEGORIAS COM PRODUTOS*/
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		/*
+		 * INCLUI RELACIONAMETO DE PRODUTOS COM CATEGORIAS*/
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		/*GRAVA CATEGORIAS NO BANCO H2*/
 		categoriaRespository.save(Arrays.asList(cat1, cat2));
 		
+		
+		/*GRAVA PRODUTOS NO BANCO H2*/
+		produtoRepository.save(Arrays.asList(p1,p2,p3));
 		
 	}
 }
